6d2a2dcc14bcc910c3af948951418cb4256ed1dd
Vicky Zheng <vickyjmzheng@gmail.com>
2021-02-02 21:47:11 -0500

    still working on how to compare two trees without loading entire tree into memory at once

--- cracking_the_code.py
+++ cracking_the_code.py
@@ -9,12 +9,12 @@ a = 5
 b = 10
 swap(a,b)
 
+# Write an algorithm to compute the number of trailing zeroes in n factorial
 def factorial(n):
 	if n == 0 or n == 1:
 		return 1
 	return n * factorial(n-1)
 
-# Write an algorithm to compute the number of trailing zeroes in n factorial
 def trailing_zeroes_factorial(n):
 	# get the number of things that multiple to something that is a factor of 10
 	# if n <= 10, then it is two trailing zeroes from 2 * 5 and 10 
@@ -33,10 +33,31 @@ def trailing_zeroes_factorial(n):
 		pow_five*=5
 
 	return num_trailing_zeroes
-print(factorial(30))
-print(trailing_zeroes_factorial(30)) # 7 (5, 10, 15, 20, 25, 30) (1, 1, 1, 1, 2, 1)
-print(factorial(50))
-print(trailing_zeroes_factorial(50)) # 12 (5, 10, 15, 20, 25, 30, 35, 40, 45, 50) (1, 1, 1, 1, 2, 1, 1, 1, 1, 2)
-print(factorial(226))
-print(trailing_zeroes_factorial(226)) 
 
+# print(factorial(30))
+print(trailing_zeroes_factorial(30)) # 7 (5, 10, 15, 20, 25, 30) (1, 1, 1, 1, 2, 1)
+# print(factorial(50))
+print(trailing_zeroes_factorial(50)) # 12 (5, 10, 15, 20, 25, 30, 35, 40, 45, 50) (1, 1, 1, 1, 2, 1, 1, 1, 1, 2)
+# print(factorial(226))
+print(trailing_zeroes_factorial(226)) # 55
+
+# A child is running up a staircase with n steps and can either hop 1, 2 or 3 steps at a time. 
+# Implement a function to count the number of ways a child can run up the stairs
+# 4 : 1, 1, 1, 1 or 1, 1, 2 or 1, 2, 1 or 2, 1, 1 or 2,2 or 1,3 or 3,1 = 7 ways 
+# number of unique ways to get from 4 should be equal to number of unique ways to traverse two steps times number of unique ways to traverse two steps
+# minus duplicates? i.e 1, 1
+def unique_steps(n):
+	if n < 1:
+		return 0
+	if n == 1:
+		return 1
+	# 1,1 or 2
+	if (n == 2):
+		return 3
+	# 1, 1, 1 or 1, 2 or 2,1 or 3
+	if (n == 3):
+		return 4
+	
+	num_unique_steps = unique_steps(n-3)
+	num_unique_steps+= unique_steps(n-2)
+	num_unique_steps+= unique_steps(n-1)
--- string_leaf_tree.py
+++ string_leaf_tree.py
@@ -7,6 +7,7 @@ class tree_node:
         self.val = val
         self.left = left
         self.right = right
+        self.visited = False
     
     def print_level(self, level):
         nodes_as_val = []
@@ -50,6 +51,71 @@ def get_tree_string(t):
 def same_tree_string(t1, t2):
     return get_tree_string(t1) == get_tree_string(t2)
 
+def is_leaf(node):
+    if node == None:
+        return False
+    if node.left == None and node.right == None:
+        return True
+
+# gets a stack of tree traversal 
+def get_next_leaf(s):
+    while (len(s) > 0 and s[-1].visited):
+        s.pop()
+    while (len(s) > 0 and not is_leaf(s[-1])):
+        if s[-1].left != None and not s[-1].left.visited:
+            s.append(s[-1].left)
+        elif s[-1].right != None and not s[-1].right.visited:  
+            s.append(s[-1].right)
+        else: 
+            s[-1].visited = True
+            s.pop()
+
+    if len(s) == 0:
+        return ""
+
+    s[-1].visited = True
+    return s[-1].val
+
+def leaf_substr(leaf1, leaf2):
+    if leaf1 == None and leaf2 == None:
+        return True
+    if leaf1 == None:
+        return False
+    if leaf2 == None:
+        return False
+    return leaf1.val.starts_with(leaf2.val)
+
+# what if the whole string can't fit into memory?
+def same_tree_string_low_mem(t1, t2):
+    if t1 == None and t2 == None:
+        return True, "" 
+    if t1 == None:
+        return False 
+    if t2 == None:
+        return False 
+    s1 = [t1]
+    s2 = [t2]
+    
+    leaf1 = get_next_leaf(s1)
+    leaf2 = get_next_leaf(s2)
+    index1 = 0
+    index2 = 0 
+    while(len(s1) > 0 and len(s2) > 0):
+        line_to_print = f"leaf1: {leaf1[index1:]} leaf2: {leaf2[index2:]}"
+        print(line_to_print)
+        # leaf2 is leaf1's prefix
+        if leaf1[index1:].startswith(leaf2[index2:]) :
+            index1 = len(leaf2) - index2
+            index2 = 0 
+            leaf2 = get_next_leaf(s2)
+        # leaf1 is leaf2's prefix 
+        elif leaf2[index2:].startswith(leaf1[index1:]) :
+            index2 = len(leaf1) - index1
+            index1 = 0 
+            leaf1 = get_next_leaf(s1)
+        else:
+            return False
+    return (index1 == len(leaf1)) and (index2 == len(leaf2))
 
 t1 = tree_node("N1")
 t1.left = tree_node("N2")
@@ -74,8 +140,24 @@ t4.left.left = tree_node("Goog")
 t4.left.right = tree_node("le")
 t4.right.left = tree_node("!")
 
-print(get_tree_string(t1))
-print(get_tree_string(t2))
-print(get_tree_string(t3))
-print(get_tree_string(t4))
+t5 = tree_node("N1")
+t5.left = tree_node("N2")
+t5.right = tree_node("N3")
+t5.left.left = tree_node("Go")
+t5.right.left = tree_node("og")
+t5.right.right = tree_node("le!")
+
+t6 = tree_node("N1")
+t6.left = tree_node("Goo")
+t6.right = tree_node("gle!")
+
+# print(get_tree_string(t1))
+# print(get_tree_string(t2))
+# print(get_tree_string(t3))
+# print(get_tree_string(t4))
+
+print(same_tree_string_low_mem(t1, t5))
+print(same_tree_string_low_mem(t1, t6))
+
+
 
