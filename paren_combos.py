ca29554cdf040c07a330b1fc13bb830bc39a72fb
Vicky Zheng <vickyjmzheng@gmail.com>
2021-02-25 16:12:34 -0500

    solved another ctci problem

---
+++ paren_combos.py
@@ -0,0 +1,38 @@
+# implement an algorithm to print all valid (e.g. properly opened and closed combinations of n paired paranthesis)
+def paren_options(num_open, num_closed):
+	options = []
+	if num_open > 0:
+		options.append('(')
+	if num_closed > num_open:
+		options.append(')')
+	return options
+
+def paren_combos(n):
+	combos = []
+	first_combo = {'combo' : '',
+					'(' : n,
+					')' : n}
+	combos.append(first_combo)
+	more_to_go = True
+	while more_to_go:
+		more_to_go = False
+		new_combos = []
+		for tup in combos:
+			string = tup['combo']
+			num_open = tup['(']
+			num_closed = tup[')']
+			options = paren_options(num_open, num_closed)
+			for option in options:
+				more_to_go = True
+				new_combo = {'combo' : string + option,
+								'(' : num_open,
+								')' : num_closed}
+				new_combo[option]-=1
+				new_combos.append(new_combo)
+		if (more_to_go):
+			combos = new_combos
+
+	for combo in combos:
+		print(combo['combo'])
+
+paren_combos(3)
